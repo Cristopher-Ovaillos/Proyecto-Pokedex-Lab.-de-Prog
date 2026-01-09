ac55e10777c488a4bbc298b1b49f347cf75c3775
guillermo-diaz <guillermo.diaz@est.fi.uncoma.edu.ar>
2026-01-08 23:52:22 -0300

    estilo de card, falta ponerle el font pixel (no anda)

--- frontend/src/constants/styles.js
+++ frontend/src/constants/styles.js
@@ -1,71 +1,73 @@
-
-
 const styles = {
+  login: {
+    container: "flex-1 bg-background  p-4 justify-center items-center",
+    title: "text-primary text-2xl font-pixel",
+    input: "w-full border-gray-300 rounded px-4 py-2 mb-4 font-pixel",
+    button:
+      "bg-secondary w-full py-3 rounded-lg shadow-sm active:opacity-80 font-pixel",
+    buttonText: "text-white text-center font-pixel font-pixel",
+    errorText: "text-error text-sm mb-4",
+  },
+  register: {
+    container: "flex-1 bg-background  p-4 justify-center items-center",
+    title: "text-primary text-2xl font-pixel",
+    input: "w-full border-gray-300 rounded px-4 py-2 mb-4 font-pixel",
+    button:
+      "bg-secondary w-full py-3 rounded-lg shadow-sm active:opacity-80 font-pixel",
+    buttonText: "text-white text-center font-pixel",
+    errorText: "text-error text-sm mb-4",
+  },
+  inicio: {
+    container:
+      "flex-1 bg-background font-pixel p-4 justify-center items-center",
+    title: "text-secondary text-2xl",
+  },
 
-    login: {
-        container: "flex-1 bg-background  p-4 justify-center items-center",
-        title: "text-primary text-2xl font-pixel",
-        input: "w-full border-gray-300 rounded px-4 py-2 mb-4 font-pixel",
-        button: "bg-secondary w-full py-3 rounded-lg shadow-sm active:opacity-80 font-pixel",
-        buttonText: "text-white text-center font-pixel font-pixel",
-        errorText: "text-error text-sm mb-4",
+  app: {
+    container: "flex-1 bg-background font-pixel",
+    title: "text-primary text-3xl font-pixel text-center my-4",
+  },
 
-    },
-    register: {
-        container: "flex-1 bg-background  p-4 justify-center items-center",
-        title: "text-primary text-2xl font-pixel",
-        input: "w-full border-gray-300 rounded px-4 py-2 mb-4 font-pixel",
-        button: "bg-secondary w-full py-3 rounded-lg shadow-sm active:opacity-80 font-pixel",
-        buttonText: "text-white text-center font-pixel font-pixel",
-        errorText: "text-error text-sm mb-4",
-    },
-    inicio: {
-        container: "flex-1 bg-background font-pixel p-4 justify-center items-center",
-        title: "text-secondary text-2xl",
-    },
+  card: {
+    touchable: "flex-1 m-1",
+    container: "bg-zinc-100 bg-sky-50 rounded-xl px-3 shadow-md border border-gray-400 items-center",
+    imageContainer:
+      "bg-white  p-20 w-24 h-24 self-center justify-center items-center rounded-xl border-neutral-400 border-[1.5px]",
+    image: "w-20 h-20",
+    infoContainer: "mt-2 items-center",
+    id: "text-emerald-400  p-1 font-bold self-start",
+    name: "text-emerald-400 text-xl py-1 font-bold capitalize font-pixel ",
+    typeContainer: "flex flex-row gap-3 justify-center mt-2 mb-2",
+    typePill: "px-3 py-1 bg-white  mr-1",
+    typeText: "text-white text-xs font-bold capitalize",
+  },
+  list: {
+    container: "px-2",
+    footer: "p-4",
+  },
 
-    app: {
-        container: "flex-1 bg-background font-pixel",
-        title: "text-primary text-3xl font-pixel text-center my-4"
-    },
+  movimientos: {
+    container:
+      "flex-1 bg-background font-pixel p-4 justify-center items-center",
+    title: "text-info text-2xl",
+  },
+  perfil: {
+    container:
+      "flex-1 bg-background font-pixel p-4 justify-center items-center",
+    title: "text-dark text-2xl",
+  },
+  crearEquipo: {
+    container:
+      "flex-1 bg-background font-pixel p-4 justify-center items-center",
+    title: "text-success text-2xl",
+  },
 
-    card: {
-        touchable: "flex-1 m-1",
-        container: "rounded-xl p-3 shadow-md",
-        imageContainer: "bg-white/30 rounded-full w-24 h-24 self-center justify-center items-center",
-        image: "w-20 h-20",
-        infoContainer: "mt-2 items-center",
-        id: "text-black/60 font-bold",
-        name: "text-white text-lg font-bold capitalize font-pixel",
-        typeContainer: "flex-row justify-center mt-2",
-        typePill: "px-3 py-1 bg-white/30 rounded-full mr-1",
-        typeText: "text-white text-xs font-bold capitalize",
-    },
-    list: {
-        container: "px-2",
-        footer: "p-4", 
-    },
-
-
-
-    movimientos: {
-        container: "flex-1 bg-background font-pixel p-4 justify-center items-center",
-        title: "text-info text-2xl",
-    },
-    perfil: {
-        container: "flex-1 bg-background font-pixel p-4 justify-center items-center",
-        title: "text-dark text-2xl",
-    },
-    crearEquipo: {
-        container: "flex-1 bg-background font-pixel p-4 justify-center items-center",
-        title: "text-success text-2xl",
-    },
-
-    drawer: {
-        logoutButton: "bg-error mr-4 px-3 py-1 rounded-md",
-        logoutButtonText: "text-white font-pixel text-sm",
-        drawerLabel: "font-pixel",
-    },
+  drawer: {
+    logoutButton: "bg-error mr-4 px-3 py-1 rounded-md",
+    logoutButtonText: "text-white font-pixel text-sm",
+    drawerLabel: "font-pixel",
+  },
+ 
 };
 
 module.exports = styles;
--- frontend/src/features/pokedex/components/PokemonCard.js
+++ frontend/src/features/pokedex/components/PokemonCard.js
@@ -1,67 +1,39 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { TYPE_COLORS } from '../../../constants/theme';
-import styles from '../../../constants/styles';
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { TYPE_COLORS } from "../../../constants/theme";
+import styles from "../../../constants/styles";
 
 const PokemonCard = ({ pokemon, onPress }) => {
-    //para mostrar el id del pokemon en vez de '1' mostrar '001'
-    const formatedId = `#${String(pokemon.id).padStart(3, '0')}`;
-    const primaryType = pokemon.types[0]?.type.name; //mirar usePOkedex
-    const cardBackgroundColor = TYPE_COLORS[primaryType];
+  //para mostrar el id del pokemon en vez de '1' mostrar '001'
+  const formatedId = `#${String(pokemon.id).padStart(3, "0")}`;
+  const primaryType = pokemon.types[0]?.type.name; //mirar usePOkedex
+  const cardBackgroundColor = TYPE_COLORS[primaryType];
 
-    console.log({pokemon});
-    return (
-        <TouchableOpacity className={styles.card.touchable} onPress={onPress}>
-            <View className={ styles.card.container}  style={{backgroundColor: cardBackgroundColor}}>
-                <View className={styles.card.imageContainer}>
-                    <Image 
-                    source={{uri:pokemon.image}}
-                    className={styles.card.image}
-                    resizeMode="contain"
-                    />
-                </View>
+  // console.log({pokemon});
+  return (
+    <TouchableOpacity className={styles.card.touchable} onPress={onPress}>
+      <View className={styles.card.container}>
+        <Text className={styles.card.id}>{formatedId}</Text>
+        <View className={styles.card.imageContainer}>
+          <Image
+            source={{ uri: pokemon.image }}
+            className={styles.card.image}
+            resizeMode="contain"
+          />
+        </View>
 
-                <View className={styles.card.infoContainer}>
-                    <Text className={styles.card.id}>
-                        {formatedId}
-                    </Text>
-                    <Text className={styles.card.name}>
-                        {pokemon.name}
-                    </Text>
+        <Text className={styles.card.name}>{pokemon.name}</Text>
 
-                </View>
-
-                <View>
-                    {
-                        pokemon.types.map(({ type }) => (
-                            <View key={type.name} className={styles.card.typePill}>
-                                <Text>
-                                    {type.name}
-                                </Text>
-                            </View>
-
-                        ))
-
-
-                    }
-
-
-
-                </View>
-
-                <View>
-
-
-                </View>
+        <View className={styles.card.typeContainer}>
+          {pokemon.types.map(({ type }) => (
+            <View key={type.name} className={styles.card.typePill} style={{backgroundColor: TYPE_COLORS[type.name]}}>
+              <Text className={styles.card.typeText}>{type.name}</Text>
             </View>
+          ))}
+        </View>
 
-
-
-
-
-        </TouchableOpacity>
-    );
-
-
-
+        <View></View>
+      </View>
+    </TouchableOpacity>
+  );
 };
 export default PokemonCard;
